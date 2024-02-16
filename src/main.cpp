6d8eb73e85b7e2678319311fee223106f4404c1e
TheoPratsRioufol <theopratsrioufol@gmail.com>
2024-02-16 14:01:42 +0100

    src main change

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,33 @@
 #include "gladiator.h"
-Gladiator* gladiator;
+Gladiator *gladiator;
 void reset();
-void setup() {
-    //instanciation de l'objet gladiator
+void setup()
+{
+    // instanciation de l'objet gladiator
     gladiator = new Gladiator();
-    //enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
+    // enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
     gladiator->game->onReset(&reset); // GFA 4.4.1
 }
 
-void reset() {
-    //fonction de reset:
-    //initialisation de toutes vos variables avant le début d'un match
+void reset()
+{
+    // fonction de reset:
+    // initialisation de toutes vos variables avant le début d'un match
     gladiator->log("Call of reset function"); // GFA 4.5.1
 }
 
-void loop() {
-    if(gladiator->game->isStarted()) { //tester si un match à déjà commencer
-        //code de votre stratégie
+void loop()
+{
+    if (gladiator->game->isStarted())
+    { // tester si un match à déjà commencer
+        // code de votre stratégie
         gladiator->log("Hello world - Game Started"); // GFA 4.5.1
-    }else {
+    }
+    else
+    {
         gladiator->log("Hello world - Game not Startd yet"); // GFA 4.5.1
     }
     delay(300);
 }
+
+// force git
