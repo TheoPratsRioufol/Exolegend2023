012f957286e342bfbd57c2403db3b984a6ec5c41
TheoPratsRioufol <theopratsrioufol@gmail.com>
2024-02-17 08:51:25 +0100

    avancee vendredi soir

--- src/main.cpp
+++ src/main.cpp
@@ -33,9 +33,8 @@
 //     delay(300);
 // }
 
-
 #include "gladiator.h"
-Gladiator* gladiator;
+Gladiator *gladiator;
 
 float kw = 1.2;
 float kv = 1.f;
@@ -50,6 +49,7 @@ double reductionAngle(double x)
         x += 2 * PI;
     return x - PI;
 }
+
 void go_to(Position cons, Position pos)
 {
     double consvl, consvr;
@@ -57,7 +57,7 @@ void go_to(Position cons, Position pos)
     double dy = cons.y - pos.y;
     double d = sqrt(dx * dx + dy * dy);
 
-    if (d > erreurPos)
+    while (d > erreurPos)
     {
         double rho = atan2(dy, dx);
         double consw = kw * reductionAngle(rho - pos.a);
@@ -68,50 +68,52 @@ void go_to(Position cons, Position pos)
 
         consvl = consv - gladiator->robot->getRobotRadius() * consw; // GFA 3.6.2
         consvr = consv + gladiator->robot->getRobotRadius() * consw; // GFA 3.6.2
-    }
-    else
-    {
-        consvr = 0;
-        consvl = 0;
-    }
 
-    gladiator->control->setWheelSpeed(WheelAxis::RIGHT, consvr, false); // GFA 3.2.1
-    gladiator->control->setWheelSpeed(WheelAxis::LEFT, consvl, false);  // GFA 3.2.1
+        gladiator->control->setWheelSpeed(WheelAxis::RIGHT, consvr, false); // GFA 3.2.1
+        gladiator->control->setWheelSpeed(WheelAxis::LEFT, consvl, false);  // GFA 3.2.1
+    }
+    consvr = 0;
+    consvl = 0;
+
+    gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0, false); // GFA 3.2.1
+    gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0, false);  // GFA 3.2.1
 }
-
-
 
 void reset();
-void setup() {
-    //instanciation de l'objet gladiator
+void setup()
+{
+    // instanciation de l'objet gladiator
     gladiator = new Gladiator();
-    //enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
+    // enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
     gladiator->game->onReset(&reset); // GFA 4.4.1
 
-    gladiator->log( "x = %f", gladiator->robot->getData().position.x);
-    gladiator->log( "y = %f", gladiator->robot->getData().position.y);
-    gladiator->log( "a = %f", gladiator->robot->getData().position.a);
+    gladiator->log("x = %f", gladiator->robot->getData().position.x);
+    gladiator->log("y = %f", gladiator->robot->getData().position.y);
+    gladiator->log("a = %f", gladiator->robot->getData().position.a);
 }
 
-void reset() {
-    //fonction de reset:
-    //initialisation de toutes vos variables avant le début d'un match
+void reset()
+{
+    // fonction de reset:
+    // initialisation de toutes vos variables avant le début d'un match
     gladiator->log("Call of reset function"); // GFA 4.5.1
 }
 
-void loop() {
-    if(gladiator->game->isStarted()) { //tester si un match à déjà commencer
-        //code de votre stratégie
-        // Position myPosition = gladiator->robot->getData().position;
-        // Position goal {0.5,1.5,0};
-        // go_to(goal, myPosition);
+void loop()
+{
+    if (gladiator->game->isStarted())
+    { // tester si un match à déjà commencer
+        // code de votre stratégie
+        Position myPosition = gladiator->robot->getData().position;
+        Position goal{1.5, 1.5, 0};
+        go_to(goal, myPosition);
 
-        gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0.5, false);
-        gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0.5, false);
-        delay(1000);
-        gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0, false);
-        gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0, false);
-        delay(1000);
+        // gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0.1, false);
+        // gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0.2, false);
+        // delay(1000);
+        // gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0, false);
+        // gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0, false);
+        // delay(1000);
     }
     delay(10);
 }
