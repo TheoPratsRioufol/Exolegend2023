c3a5ef2b44e20b741868fce1e53c2cd3eeb340ef
Joseph <joseph.guerin@student-cs.fr>
2025-03-06 11:40:13 +0100

    Corrigé les problèmes de reset

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 Gladiator* gladiator;
 
 
+// float squareSize;
+bool reached;
+Position center = {0,0};
+Position current_pos = {0,0};
+
+
 void reset();
 
 void setup() {
@@ -15,10 +21,40 @@ void setup() {
     gladiator->game->onReset(&reset); // GFA 4.4.1
 }
 
+void update_target() {
+     // Obtenir le carré le plus proche du robot dans le labyrinthe
+     const MazeSquare* nearestSquare = gladiator->maze->getNearestSquare();
+     float squareSize = gladiator->maze->getSquareSize();
+
+     if (nearestSquare->eastSquare != nullptr) {
+         center.x = (nearestSquare->eastSquare->i + 0.5) * squareSize;
+         center.y = (nearestSquare->eastSquare->j + 0.5) * squareSize;
+     } else if (nearestSquare->southSquare != nullptr) {
+         center.x = (nearestSquare->southSquare->i + 0.5) * squareSize;
+         center.y = (nearestSquare->southSquare->j + 0.5) * squareSize;
+     } else if (nearestSquare->westSquare != nullptr) {
+         center.x = (nearestSquare->westSquare->i + 0.5) * squareSize;
+         center.y = (nearestSquare->westSquare->j + 0.5) * squareSize;
+     } else {
+         center.x = (nearestSquare->northSquare->i + 0.5) * squareSize;
+         center.y = (nearestSquare->northSquare->j + 0.5) * squareSize;
+     }
+}
+
 void reset() {
     //fonction de reset:
     //initialisation de toutes vos variables avant le début d'un match
     gladiator->log("Call of reset function"); // GFA 4.5.1
+
+    const MazeSquare* nearestSquare = gladiator->maze->getNearestSquare();
+    float squareSize = gladiator->maze->getSquareSize();
+    // reset the motors
+    reset_motors(nearestSquare, squareSize, gladiator);
+    
+    update_target();
+
+    current_pos = gladiator->robot->getData().position;
+    reached = false;
 }
 
 
@@ -33,34 +69,13 @@ void loop() {
         gladiator->log("Hello world - Game Started"); // GFA 4.5.1
 
         while(true) {
-            // Position target_pos;
-
-            // Obtenir le carré le plus proche du robot dans le labyrinthe
-            const MazeSquare* nearestSquare = gladiator->maze->getNearestSquare();
-            float squareSize = gladiator->maze->getSquareSize();
-
-            Position center = {0,0};
-
-            if (nearestSquare->eastSquare != nullptr) {
-                center.x = (nearestSquare->eastSquare->i + 0.5) * squareSize;
-                center.y = (nearestSquare->eastSquare->j + 0.5) * squareSize;
-            } else if (nearestSquare->southSquare != nullptr) {
-                center.x = (nearestSquare->southSquare->i + 0.5) * squareSize;
-                center.y = (nearestSquare->southSquare->j + 0.5) * squareSize;
-            } else if (nearestSquare->westSquare != nullptr) {
-                center.x = (nearestSquare->westSquare->i + 0.5) * squareSize;
-                center.y = (nearestSquare->westSquare->j + 0.5) * squareSize;
-            } else {
-                center.x = (nearestSquare->northSquare->i + 0.5) * squareSize;
-                center.y = (nearestSquare->northSquare->j + 0.5) * squareSize;
-            }
-
-            // target_pos = center;
+            update_target();
 
             bool reached = false;
             while (!reached) {
-                Position current_pos = gladiator->robot->getData().position;
+                current_pos = gladiator->robot->getData().position;
                 
+                gladiator->log("( %f; %f )", center.x , center.y);
                 reached = go_to(center, current_pos, gladiator);
             }
         }
@@ -68,5 +83,5 @@ void loop() {
     }else {
         gladiator->log("Hello world - Game not Started yet"); // GFA 4.5.1
     }
-    delay(300);
+    // delay(300);
 }
